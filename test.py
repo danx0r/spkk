6824801a6658de0837b265c76668db48909b4acf
danx0r <dan.miller@eye0.com>
2020-12-15 16:24:48 -0800

    test straight record

--- test.py
+++ test.py
@@ -1,9 +1,44 @@
 import speech_recognition as sr
+from scipy.io.wavfile import write
+import sounddevice as sd
 
-r=sr.Recognizer()
-wav=sr.AudioFile("test.wav")
-with wav as source:
-        aud = r.record(source)
+# RCOG = "recognize_google"
+RCOG = "recognize_google_cloud"
+# RCOG = "recognize_sphinx"
+DEV = 2
 
-txt=r.recognize_sphinx(aud)
-print (txt)
+# r=sr.Recognizer()
+# wav=sr.AudioFile("test.wav")
+# with wav as source:
+#         aud = r.record(source)
+#
+# txt=r.recognize_sphinx(aud)
+# print (txt)
+
+r = sr.Recognizer()
+rcog = getattr(r, RCOG)
+mic=sr.Microphone(device_index=DEV, chunk_size=409)
+
+while 1-1:
+    with mic as src:
+        aud = r.listen(src)
+
+    txt=rcog(aud)
+    print (txt)
+
+fs = 44100  # Sample rate
+seconds = 5  # Duration of recording
+
+raw = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype="int16")
+sd.wait()  # Wait until recording is finished
+print (type(raw), raw[0][0])
+write('out.wav', fs, raw)
+aud = sr.AudioData(raw, 44100, 2)
+
+# wav=sr.AudioFile("out2.wav")
+# with wav as source:
+#         aud = r.record(source)
+
+print ("aud:", aud)
+txt = rcog(aud)
+print(txt)
