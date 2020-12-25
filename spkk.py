b292364f405b29381d17d061ecd076bee0932365
danx0r <dan.miller@eye0.com>
2020-12-24 18:23:33 -0800

    x deletes

--- spkk.py
+++ spkk.py
@@ -4,8 +4,8 @@ from scipy.io.wavfile import write
 import sounddevice as sd
 
 # RCOG = "recognize_google"
-RCOG = "recognize_google_cloud"
-# RCOG = "recognize_sphinx"
+# RCOG = "recognize_google_cloud"
+RCOG = "recognize_sphinx"
 DEV = 2
 
 # r=sr.Recognizer()
@@ -31,18 +31,21 @@ fs = 44100  # Sample rate
 seconds = 60  # Duration of recording
 
 print("Enter to start recording", file=sys.stderr)
+cmd = input().strip()
 while True:
-    cmd = input().strip()
-    if cmd:
-        break
     print ("RECORDING...", file=sys.stderr)
     raw = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype="int16")
     cmd = input().strip()
     sd.stop()
     print ("STOPPED", file=sys.stderr)
-    if cmd:
-        break
     aud = sr.AudioData(raw, 44100, 2)
     txt = rcog(aud).strip()
     print(txt, file=sys.stderr, end="")
-    print(txt)
+    cmd = input().strip()
+    if cmd=='q':
+        print(txt)
+        break
+    elif cmd=="":
+        print(txt)
+    else:
+        print("\nDELETED", file=sys.stderr)
