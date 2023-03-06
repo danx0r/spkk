800d1a32bfe78377af586eb9cba1c1cbeb36b4ca
danx0r <dan.miller@eye0.com>
2023-03-05 18:07:19 -0800

    rework file

--- spkk.py
+++ spkk.py
@@ -4,46 +4,44 @@ import sounddevice as sd
 # import numpy as np
 import whisper
 
-print("Loading model", file=sys.stderr)
+fs = 44100  # Sample rate
+seconds = 60  # Duration of recording
+DEV = 2 # Audio input device
+
+print("Loading model")
 wmodel = whisper.load_model("base")
-print("Model loaded", file=sys.stderr)
-
-DEV = 2
-
+print("Model loaded")
 
 def recognize_whisper(wavfile):
     result = wmodel.transcribe(wavfile, fp16=False)
-    # print(result["text"])
     return result["text"]
 
-fs = 44100  # Sample rate
-seconds = 60  # Duration of recording
-
-print("Enter to start recording", file=sys.stderr)
+print("Enter to start recording")
 cmd = input().strip()
+fout = open(sys.argv[1], 'w')
+
 while True:
     t = time.time()
-    print ("RECORDING...", file=sys.stderr)
+    print ("RECORDING...")
     raw = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype="int16")
     cmd = input().strip()
     sd.stop()
     sd.wait()
     t = time.time()-t-.1
-    # print (t, "Seconds")
-    print ("STOPPED", file=sys.stderr)
+    print ("STOPPED")
     aud = sr.AudioData(raw[:int(t)*fs], fs, 2)
     f=open("__spkk__.wav", 'wb')
     f.write(aud.get_wav_data())
     f.close()
     txt = recognize_whisper("__spkk__.wav")
     txt = txt.strip()
-    print (txt, file=sys.stderr, end="")
+    print (txt, end="")
     cmd = input().strip()
     if cmd=='q':
-        print(txt)
+        print(txt, file=fout)
         break
     elif cmd=='x':
-        print("\nDELETED", file=sys.stderr)
+        print("\nDELETED")
     elif cmd=='e':
         f = open("__spkk_edit__", 'w')
         f.write(txt)
@@ -52,11 +50,11 @@ while True:
         f = open("__spkk_edit__")
         txt = f.read()
         f.close()
-        print (txt, file=sys.stderr)
+        print (txt, file=fout)
         print (txt)
     elif cmd == "":
-        print(txt, end=" ")
+        print(txt, end=" ", file=fout)
     else:
         txt = txt[:-1] + cmd
-        print(txt, file=sys.stderr, end="")
-        print (txt)
+        print (txt, file=fout)
+        print(txt, end="")
